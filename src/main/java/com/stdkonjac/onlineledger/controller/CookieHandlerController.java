419a12ad87f330b06cb2661183390d1f508a3918
stdKonjac <918836143@qq.com>
2019-12-25 11:41:41 +0800

    1. 修复时区导致的日期不同步问题

--- src/main/java/com/stdkonjac/onlineledger/controller/CookieHandlerController.java
+++ src/main/java/com/stdkonjac/onlineledger/controller/CookieHandlerController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
 import java.util.Map;
 
 @RestController
